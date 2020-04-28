25589e1befe1f351a3c88103b8fedc3e77160540
Davide Saporita <davide.saporita@gmail.com>
2020-04-28 15:39:32 +0200

    Handlebars Template Test

--- script/main.js
+++ script/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-	//Code
+	
+	// Vars
+	var apiUrl = 'https://flynn.boolean.careers/exercises/api/array/music';
+
+	// Refs
+	var container = $('.cds-container');
+
+	// Handlebars init
+	var cdTemplate = Handlebars.compile($('#box-template').html());
+
+	// Handlebars test
+	var data = {
+		imgUrl: 'https://upload.wikimedia.org/wikipedia/en/9/9f/Ten_Summoner%27s_Tales.jpg',
+		title: 'Titolo',
+		author: 'Autore',
+		year: '1984'
+	}
+	
+	container.append(cdTemplate(data));
+
 });
