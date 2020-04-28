7a9c2466ed9e090f80c9eeb5c63a720add15520f
Davide Saporita <davide.saporita@gmail.com>
2020-04-28 16:51:10 +0200

    Bonus function (select box) completed

--- script/main.js
+++ script/main.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 	
-	// Vars
+	// Vars + Refs + Handlebars init
 	var apiUrl = 'https://flynn.boolean.careers/exercises/api/array/music';
-
-	// Refs
 	var container = $('.cds-container');
-
-	// Handlebars init
+	var logoContainer = $('.container > img');
 	var cdTemplate = Handlebars.compile($('#box-template').html());
+	var selectTemplate = Handlebars.compile($('#select-template').html());
+	var optionsTemplate = Handlebars.compile($('#options-template').html());
+	var genresList = [];
 
 	// API call
 	$.ajax({
 		url: apiUrl,
 		method: 'GET',
 		success: (data) => {
-			disks = data.response;
-			disks.forEach(item => {
+			albums = data.response;
+			albums.forEach(item => {
 				var cd = {
 					imgUrl: item.poster,
 					title:  item.title,
@@ -23,10 +23,56 @@ $(document).ready(function() {
 					year:   item.year
 				}
 				container.append(cdTemplate(cd));
-			});
-		},
+				if(!genresList.includes(item.genre)) {
+					genresList.push(item.genre);
+				}
+			}, setTimeout(createSelect, 1000)); // Callback function at the end of albums.forEach to create a selectbox
+		}, 
 		error: () => console.log('Error')
-
 	}); // End of API call
+
+	// Select
+	$(document).on('change', '#genre-select', function() {
+		// API call
+		var self = $(this).val();
+		
+		$.ajax({
+			url: apiUrl,
+			method: 'GET',
+			success: (data) => {
+				container.html('');
+				albums = data.response;
+				albums.forEach(item => {
+					if(item.genre === self){
+						var cd = {
+							imgUrl: item.poster,
+							title:  item.title,
+							author: item.author,
+							genre:  item.genre,
+							year:   item.year
+						}
+						container.append(cdTemplate(cd));
+					}
+				}); // end of albums.forEach
+			}, 
+			error: () => console.log('Error')
+		}); // End of API call
+	}); // End of change event on select
+
+
+	// Functions
+	function createSelect() {
+		logoContainer.parent().append(selectTemplate);
+		createOptions('Select a genre', 1);
+		genresList.forEach(item => { createOptions(item); });
+	}
+
+	function createOptions(text, first) {
+		var data = { option: text };
+		if(first === 1) data.selected = 'selected';
+		else			data.value = text;
+		$('select').append(optionsTemplate(data));
+	}
+	
 
 }); // End of ready function
