52506006984f25f12af3148db713aecfb37873b6
Davide Saporita <davide.saporita@gmail.com>
2020-04-28 17:00:11 +0200

    Fixed select box + Added "all genres"

--- script/main.js
+++ script/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 	var optionsTemplate = Handlebars.compile($('#options-template').html());
 	var genresList = [];
 
-	// API call
+	// API call to populate container first time
 	$.ajax({
 		url: apiUrl,
 		method: 'GET',
@@ -20,6 +20,7 @@ $(document).ready(function() {
 					imgUrl: item.poster,
 					title:  item.title,
 					author: item.author,
+					genre:  item.genre,
 					year:   item.year
 				}
 				container.append(cdTemplate(cd));
@@ -35,15 +36,14 @@ $(document).ready(function() {
 	$(document).on('change', '#genre-select', function() {
 		// API call
 		var self = $(this).val();
-		
 		$.ajax({
 			url: apiUrl,
 			method: 'GET',
 			success: (data) => {
-				container.html('');
+				refresh(container);
 				albums = data.response;
 				albums.forEach(item => {
-					if(item.genre === self){
+					if(item.genre === self || self === 'all') {
 						var cd = {
 							imgUrl: item.poster,
 							title:  item.title,
@@ -63,16 +63,24 @@ $(document).ready(function() {
 	// Functions
 	function createSelect() {
 		logoContainer.parent().append(selectTemplate);
-		createOptions('Select a genre', 1);
+		createOptions('All genres', 1);
 		genresList.forEach(item => { createOptions(item); });
 	}
 
 	function createOptions(text, first) {
 		var data = { option: text };
-		if(first === 1) data.selected = 'selected';
-		else			data.value = text;
+		if(first === 1) {
+			data.value = 'all';
+			data.selected = 'selected';
+		} else {
+			data.value = text;
+		}			
 		$('select').append(optionsTemplate(data));
 	}
-	
 
+	function refresh(ref) {
+		ref.html('');
+	}
+
+	
 }); // End of ready function
